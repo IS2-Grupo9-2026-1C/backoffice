da3710cf48b6f839aef6f0d360f6991bfd68add3
fernandezfacu <facufer189@gmail.com>
2026-05-26 00:50:45 +0000

    ops: add ci workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, '.', '');
 
   return {
     base: env.VITE_BASE_PATH ?? '/',
     plugins: [react()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        '@': '/src',
       },
     },
     server: {
